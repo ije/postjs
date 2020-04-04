4145a832981907d50a554ec63f041a367e1d6456
Je <git@iamje.com>
2020-04-05 04:46:58 +0800

    chore: update examples

--- examples/transition/components/nav.tsx
+++ examples/transition/components/nav.tsx
@@ -1,19 +1,19 @@
-import { NavLink, Transition } from '@postjs/core'
+import { fadeIn, fadeOut, NavLink } from '@postjs/core'
 import React from 'react'
 
 const linkStyle = { color: 'inherit', fontSize: 18, textDecoration: 'none', opacity: 0.9 }
 const linkActiveStyle = { borderBottom: '1px solid rgba(255,255,255,0.75)' }
 const dotStyle = { padding: '0 9px', opacity: 0.5, fontSize: 18 }
-const fade = Transition.fade(900)
+const transition = { enter: fadeIn(900), exit: fadeOut(900) }
 
 export default function Nav() {
     return (
         <nav style={{ margin: 50 }}>
-            <NavLink to="/r" style={linkStyle} activeStyle={linkActiveStyle} effect={fade}>R</NavLink>
+            <NavLink to="/r" style={linkStyle} activeStyle={linkActiveStyle} transition={transition}>R</NavLink>
             <span style={dotStyle}>&middot;</span>
-            <NavLink to="/g" style={linkStyle} activeStyle={linkActiveStyle} effect={fade}>G</NavLink>
+            <NavLink to="/g" style={linkStyle} activeStyle={linkActiveStyle} transition={transition}>G</NavLink>
             <span style={dotStyle}>&middot;</span>
-            <NavLink to="/b" style={linkStyle} activeStyle={linkActiveStyle} effect={fade}>B</NavLink>
+            <NavLink to="/b" style={linkStyle} activeStyle={linkActiveStyle} transition={transition}>B</NavLink>
         </nav>
     )
 }
