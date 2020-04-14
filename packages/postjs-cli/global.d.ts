3cbe9724757ec68cfdd02ed1c68107dddf609e15
Je <git@iamje.com>
2020-04-14 17:50:05 +0800

    chore: update global typings

--- packages/postjs-cli/global.d.ts
+++ packages/postjs-cli/global.d.ts
@@ -1,6 +1,7 @@
 declare module 'webpack-virtual-modules' {
-    import webpack from 'webpack'
+    import webpack from 'webpack';
     export default class IVirtualModulesPlugin extends webpack.Plugin {
+        constructor(modules?: Record<string, string>)
         writeModule(filePath: string, contents: string): void
     }
 }
