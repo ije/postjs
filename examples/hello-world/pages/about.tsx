39601e5e0119e3fdf829ad0258a75ae5a5077580
Je <git@iamje.com>
2020-05-21 18:00:27 +0800

    chore: update examples

--- examples/hello-world/pages/about.tsx
+++ examples/hello-world/pages/about.tsx
@@ -1,6 +1,6 @@
 import React from 'https://cdn.pika.dev/react'
 
-export default function Home() {
+export default function About() {
     return (
         <div style={{ margin: 60 }}>
             <p>The radical new Post-Front-End Framework...</p>
