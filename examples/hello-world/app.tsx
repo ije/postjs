7337b9684a77f41100e691c4281d7ac537532b26
Je <git@iamje.com>
2020-09-05 02:08:18 +0800

    chore: update hello example

--- examples/hello-world/app.tsx
+++ examples/hello-world/app.tsx
@@ -1,8 +1,12 @@
 import React, { Fragment, PropsWithChildren } from 'react'
+import {Head  } from 'https://postjs.io/head.ts'
 
 export default function App({ children }: PropsWithChildren<{}>) {
     return (
         <Fragment>
+            <Head>
+                <title>Hello World - postjs</title>
+            </Head>
             {children}
         </Fragment>
     )
